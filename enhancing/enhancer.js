cffa423374605ad1ddb2166f3a551cd4a69919f2
Maria Trujillo <mptruj015@gmail.com>
2020-05-20 23:16:20 +0200

    tested succeed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,35 @@ module.exports = {
   get,
 };
 
+
+//The item's enhancement increases by 1.
+//If the item enhancement level is 20, the enhancement level is not changed.
+//The durability of the item is not changed.
 function succeed(item) {
-  return { ...item };
+   if(item.enhancemnet >=0 && item.enhancement < 20){
+     return item.enhancement + 1;
+   } else {
+     return item.enhancement;
+   }
 }
+
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement >= 0 && item.enhancement < 15){
+    return item.durability - 5;
+  }else if (item.enhancement >= 15){
+    return item.durability + 10;
+  } else if (item.enhancement > 16){
+    return item.durability-1;
+  }
 }
 
+//accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
 function repair(item) {
   return { ...item };
 }
 
+//stretch
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
-const enhancer = require('./enhancer.js');
+const {succeed} = require('./enhancer.js');
+
+
 // test away!
+describe('enhancer', () => {
+    describe('succeed', () => {
+        it("should increase enhancement by 1 if enhancement is < 20", () => {
+            let item = {
+                name: "horcrux",
+                enhancement: 0,
+                durability: 0
+            }
+
+            // expect(succeed(item)).toBe(1);
+            //expect(succeed(item)).toBe(21);
+            expect(succeed(item)).toBe(0)
+        })
+    })
+
+    // decribe('fail', () => {
+    //     it("should increase or decrease the item's durability depending on the enhancment amount", () => {
+    //         let item = {
+
+    //         }
+    //     })
+    // })
+})
